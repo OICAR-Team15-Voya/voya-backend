35b777cbf91b9372a3de3764cdd4226caeb6a0ca
bruno <bruno.bogdan02@gmail.com>
2026-03-31 20:33:51 +0200

    initial commit

---
+++ Voya-Backend/src/main/java/hr/algebra/voyabackend/VoyaBackendApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.voyabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoyaBackendApplication.class, args);
+    }
+
+}
