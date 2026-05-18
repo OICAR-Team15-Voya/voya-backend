c0d936c59815eab343f31c3859c483a7c8d61800
bruno <bruno.bogdan02@gmail.com>
2026-05-18 15:59:14 +0200

    Notification Service created sendNewReservationEmail method implemented

--- Voya-Backend/src/main/java/hr/algebra/voyabackend/repository/UserRepository.java
+++ Voya-Backend/src/main/java/hr/algebra/voyabackend/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package hr.algebra.voyabackend.repository;
 
 import hr.algebra.voyabackend.model.User;
+import hr.algebra.voyabackend.model.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -17,4 +19,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     //    Custom methods:
     Optional<User> findByEmail(String email);
+    List<User> findByRole(Role role);
 }
---
+++ Voya-Backend/src/main/java/hr/algebra/voyabackend/service/EmailService.java
@@ -0,0 +1,30 @@
+package hr.algebra.voyabackend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String voyaGmailAddress;
+
+    public void sendEmail(String to, String subject, String text){
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        // setting default sender
+        message.setFrom(voyaGmailAddress);
+
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        mailSender.send(message);
+    }
+}
